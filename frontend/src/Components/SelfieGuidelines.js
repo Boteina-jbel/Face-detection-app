2f202b09d83a1658327685718c4a151843923a55
Boteina <boteinajbel@gmail.com>
2025-03-07 23:53:15 +0000

    save devs

--- frontend/src/Components/SelfieGuidelines.js
+++ frontend/src/Components/SelfieGuidelines.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import '../Features.css';
-
 
 const SelfieGuidelines = () => {
     return (
         <div>
-            <h2>Hello world</h2>
+            <h2>TAKE YOUR SELFIE</h2>
+            <p>For an accurate skin analysis:</p>
+            <ul>
+                <li>Pull hair back, remove makeup and glasses</li>
+                <li>Use the front-facing camera and minimize shadows</li>
+                <li>Center entire face and have a neutral expression</li>
+            </ul>
+            <button className="s">
+                GET STARTED
+            </button>
+            <button className="s">
+                GET STARTED
+            </button>
         </div>
     );
 };
